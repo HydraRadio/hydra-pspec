d315f81efa289bff4378aeff25aee7228e61bb04
jburba <jacob.burba@manchester.ac.uk>
2023-05-12 16:58:42 +0100

    Added chi-squared and natural log posterior values per iteration to verbose print statements

--- hydra_pspec/pspec.py
+++ hydra_pspec/pspec.py
@@ -217,9 +217,9 @@ def gcr_fgmodes(
 
     # Return sample
     if verbose:
-        print(f"{time.time() - st:<14.1f}", end="")
-        print(f"{info.mean():<7.1f}", end="")
-        print(f"{residuals.mean():.2e}")
+        print(f"{time.time() - st:<12.1f}", end="")
+        print(f"{info.mean():<8.1f}", end="")
+        print(f"{residuals.mean():<12.2e}", end="")
     return samples
 
 
@@ -322,11 +322,36 @@ def gibbs_step_fgmodes(
     signal_cr = cr[:, : -fgmodes.shape[1]]
     fg_amps = cr[:, -fgmodes.shape[1] :]
 
+    if verbose:
+        # Full model of data is sum of EoR (GCR) + FG model
+        model = signal_cr + fg_amps @ fgmodes.T  # np.einsum('ijk,lk->ijl', fg_amps, fgmodes)
+        # Chi-squared is computed as the sum of ( |data - model| / noise )^2,
+        # i.e. as a sum of standard normal random variables.
+        # FIXME: this will need to be changed to account for time-dependent
+        # flags (i.e. when we have a different N per time).
+        chisq = np.sum(np.abs(vis - model)**2 * Ninv.diagonal()[None, :])
+        chisq /= vis.size  # Chi-squared per degree of freedom
+        print(f"{chisq:<9.3f}", end="")
+
     # (2) Sample EoR signal power spectrum (and also convert to equivalent
     # covariance matrix sample)
     ps_sample = sample_S(s=signal_cr, prior=ps_prior)
     # The factor of 1/Nfreqs**2 here is an FFT normalization
     S_sample = covariance_from_pspec(ps_sample / Nfreqs**2, fourier_op)
+
+    if verbose:
+        # Log posterior
+        # Each time is treated as an independent sample.  So, the joint
+        # log posterior for all times is the sum of the individual log
+        # posteriors for each time.
+        # WARNING: np.linalg.inv should be avoided for general, dense matrices.
+        # S_sample should be diagonally dominant and thus this should be okay.
+        Sinv = np.linalg.inv(S_sample)
+        ln_post = np.sum(np.diagonal(
+            -(vis - model).conj() @ Ninv @ (vis - model).T
+            - signal_cr.conj() @ Sinv @ signal_cr.T
+        ))
+        print(f"{ln_post.real:<12.1f}")
 
     # Return samples
     return signal_cr, S_sample, ps_sample, fg_amps
@@ -437,11 +462,11 @@ def gibbs_sample_with_fg(
 
     # Loop over iterations
     if verbose:
-        print("Iteration   Elapsed [s]   Info   |Ax - b|")
-        print("---------   -----------   ----   --------")
+        print("Iter     Time [s]    Info    |Ax - b|    Chisq    ln Post")
+        print("-----    --------    ----    --------    -----    -------")
     for i in range(Niter):
         if verbose:
-            print(f"{i+1:<8d}", end="    ")
+            print(f"{i+1:<9d}", end="")
 
         # Do Gibbs iteration
         signal_cr[i], signal_S, signal_ps[i], fg_amps[i] = gibbs_step_fgmodes(
