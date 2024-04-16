289375c383357188a15b8f9a14affc3d4ab9732a
Jacob Burba <jacob.burba@manchester.ac.uk>
2024-04-16 17:22:37 +0100

    Finish baseline MPI modifications
    The driver script `run-hydra-pspec.py` should now be capable of running with MPI parallelization over the baseline axis.  A suitable test dataset with data for multiple baselines still needs to be added.

--- run-hydra-pspec.py
+++ run-hydra-pspec.py
@@ -23,14 +23,28 @@ from hydra_pspec.utils import (
     add_mtime_to_filepath
 )
 
+try:
+    from mpi4py import MPI
+
+    HAVE_MPI = True
+    comm = MPI.COMM_WORLD
+    size = comm.Get_size()
+    rank = comm.Get_rank()
+except ImportError:
+    HAVE_MPI = False
+    # This can be made more robust
+    import sys
+    print("This script requires MPI.  Exiting.")
+    sys.exit()
 
 parser = ArgumentParser()
 parser.add_argument(
     "--ant_str",
     type=str,
     default="cross",
-    help="Antenna pair joined by an underscore, e.g. '1_11' or '12_14'.  Used "
-         "via the `ant_str` kwarg in `pyuvdata.UVData.select`."
+    help="Comma delimited list of antenna pairs joined by underscores, e.g. "
+         "'1_11,12_14'.  Used via the `ant_str` kwarg in "
+         "`pyuvdata.UVData.select`."
 )
 parser.add_argument(
     "--sigcov0",
@@ -253,139 +267,209 @@ def check_load_path(fp):
             return fp_is_dir, data
 
 
+if rank == 0:
+    if "config" in args.__dict__:
+        print(f"Loading config file {str(args.config[0])}", end="\n\n")
+    pprint(args.__dict__)
+    print()
 
-if "config" in args.__dict__:
-    print(f"Loading config file {str(args.config[0])}", end="\n\n")
-pprint(args.__dict__)
+    if not args.file_paths:
+        print('Must pass file(s) to analyze via --file_paths.  Exiting.')
+        sys.exit()
+    else:
+        file_paths = sorted([Path(fp) for fp in args.file_paths])
+    nfiles = len(file_paths)
+    print(f"Reading {nfiles} file(s)")
 
-if not args.file_paths:
-    print('Must pass file(s) to analyze via --file_paths.  Exiting.')
-    sys.exit()
-else:
-    file_paths = sorted([Path(fp) for fp in args.file_paths])
-nfiles = len(file_paths)
-print(f"\nReading {nfiles} file(s)")
+    uvd = UVData()
+    if args.freq_range:
+        uvd.read(file_paths[0], read_data=False)
+        freqs_in = uvd.freq_array
+        if not uvd.use_future_array_shapes:
+            # Remove the Nspws axis
+            freqs_in = freqs_in[0]
+        freqs_in = Quantity(freqs_in, unit="Hz")
+        freqs_to_keep = filter_freqs(args.freq_range, freqs_in)
+        freqs_to_keep = freqs_to_keep.to("Hz").value
+    else:
+        freqs_to_keep = None
+    uvd.read(file_paths, ant_str=args.ant_str, frequencies=freqs_to_keep)
+    uvd.conjugate_bls()
+    if args.file_paths:
+        # Sum XX and YY polarizations to obtain pseudo-Stokes I (pI)
+        # visibilities and store them in the XX polarization
+        uvd = form_pseudo_stokes_vis(uvd)
 
-uvd = UVData()
-if args.freq_range:
-    uvd.read(file_paths[0], read_data=False)
-    freqs_in = uvd.freq_array
+    Nfreqs = uvd.Nfreqs
+    freqs = uvd.freq_array
     if not uvd.use_future_array_shapes:
-        # Remove the Nspws axis
-        freqs_in = freqs_in[0]
-    freqs_in = Quantity(freqs_in, unit="Hz")
-    freqs_to_keep = filter_freqs(args.freq_range, freqs_in)
-    freqs_to_keep = freqs_to_keep.to("Hz").value
-else:
-    freqs_to_keep = None
-uvd.read(file_paths, ant_str=args.ant_str, frequencies=freqs_to_keep)
-uvd.conjugate_bls()
-if args.file_paths:
-    # Sum XX and YY polarizations to obtain pseudo-Stokes I (pI) visibilities
-    # and store them in the XX polarization
-    uvd = form_pseudo_stokes_vis(uvd)
-antpair = uvd.get_antpairs()[0]
-bl_str = f"{antpair[0]}-{antpair[1]}"
-Nfreqs = uvd.Nfreqs
+        freqs = freqs[0]
+    freqs = Quantity(freqs, unit="Hz")
+    freq_str = (
+        f"{freqs.min().to('MHz').value:.3f}-"
+        + f"{freqs.max().to('MHz').value:.3f}MHz"
+    )
 
-freqs = uvd.freq_array
-if not uvd.use_future_array_shapes:
-    freqs = freqs[0]
-freqs = Quantity(freqs, unit="Hz")
-freq_str = (
-    f"{freqs.min().to('MHz').value:.3f}-"
-    + f"{freqs.max().to('MHz').value:.3f}MHz"
-)
-
-# Get visibility data with shape (Ntimes, Nfreqs)
-# pI visibilities stored in the XX polarization
-d = uvd.get_data(antpair + ("xx",), force_copy=True)
-
-bl_data_shape = d.shape
-cov_ff_shape = (Nfreqs, Nfreqs)
-fgmodes_shape = (Nfreqs, args.Nfgmodes)
-
-if args.flags:
-    flags_path = Path(args.flags)
-    flags_path_is_dir, flags = check_load_path(flags_path)
-    if flags_path_is_dir:
-        bl_flags_path = flags_path / bl_str / args.flags_file
-        flags = np.load(bl_flags_path)
-    check_shape(flags.shape, bl_data_shape, desc=f"flags")
-else:
-    # FIXME: there is nothing currently in place to handle flags which differ
-    # between XX and YY polarizations in `form_pseudo_stokes_vis`.  Differing
-    # flags should be accounted for in the future, possibly via Nsamples.
-    flags = uvd.get_flags(antpair + ("xx",))
-
-if args.nsamples:
-    nsamples_path = Path(args.nsamples)
-    nsamples_path_is_dir, nsamples = check_load_path(nsamples_path)
-    if nsamples_path_is_dir:
-        bl_nsamples_path = nsamples_path / bl_str / args.nsamples_file
-        nsamples = np.load(bl_nsamples_path)
-    check_shape(nsamples.shape, bl_data_shape, desc=f"nsamples")
-else:
-    nsamples = None
-
-if args.noise:
-    noise_path = Path(args.noise)
-    noise_path_is_dir, noise = check_load_path(noise_path)
-    if noise_path_is_dir:
-        bl_noise_path = noise_path / bl_str / args.noise_file
-        noise = np.load(bl_noise_path)
-    check_shape(noise.shape, bl_data_shape, desc=f"noise")
-    if nsamples is not None:
-        noise /= np.sqrt(nsamples)
-    # If passing an array of noise, there is assumed to be no noise in the data
-    d += noise
-
-if args.sigcov0:
-    sigcov0_path = Path(args.sigcov0)
-    sigcov0_path_is_dir, sigcov0 = check_load_path(sigcov0_path)
-    if sigcov0_path_is_dir:
-        bl_sigcov0_path = sigcov0_path / bl_str / args.sigcov0_file
-        sigcov0 = np.load(bl_sigcov0_path)
-    check_shape(sigcov0.shape, cov_ff_shape, desc="signal covariance")
-else:
-    sigcov0 = np.eye(Nfreqs)
-
-if args.noise_cov:
-    noise_cov_path = Path(args.noise_cov)
-    noise_cov_path_is_dir, noise_cov = check_load_path(noise_cov_path)
-    if noise_cov_path_is_dir:
-        bl_noise_cov_path = noise_cov_path / bl_str / args.noise_cov_file
-        noise_cov = np.load(bl_noise_cov_path)
-    check_shape(noise_cov.shape, cov_ff_shape, desc="noise covariance")
-    Ninv = np.linalg.inv(noise_cov)
-else:
-    Ninv = np.eye(Nfreqs)
-
-if args.fgmodes:
-    fgmodes_path = Path(args.fgmodes)
-    fgmodes_path_is_dir, fgmodes = check_load_path(fgmodes_path)
-    if fgmodes_path_is_dir:
-        if not args.fgmodes_file:
-            # Look for a file with the default filename from
-            # hydra-pspec/scripts/calc-vis-cov-matrices.py
-            fgmodes_path = fgmodes_path / bl_str / f"evecs-{freq_str}.npy"
+    # Output file set up
+    out_dir = Path(args.out_dir)
+    if not args.dirname:
+        out_dir /= f"results-{freq_str}-Niter-{args.Niter}"
+    else:
+        if args.map_estimate:
+            out_dir /= args.dirname + "-map-estimate"
         else:
-            fgmodes_path = fgmodes_path / bl_str / args.fgmodes_file
-        fgmodes = np.load(fgmodes_path)
-    fgmodes = fgmodes[:, :args.Nfgmodes]
-    check_shape(fgmodes.shape, fgmodes_shape, desc="fgmodes")
+            out_dir /= args.dirname
+    if out_dir.exists() and not args.clobber:
+        # Check for existing output files to avoid overwriting
+        add_mtime_to_filepath(out_dir)
+    out_dir.mkdir(exist_ok=True, parents=True)
+    print(f"\nWriting output(s) to {out_dir.absolute()}", end="\n\n")
+    # Catalog git version
+    try:
+        git_info = get_git_version_info()
+    except:
+        git_info = ''
+    with open(out_dir / "git.json", "w") as f:
+        json.dump(git_info, f)
+    # Catalog command line arguments
+    parser.save(args, out_dir / "args.json", format="json", skip_none=False)
+    if "SLURM_JOB_ID" in os.environ:
+        # If running in SLURM, create empty file named with the SLURM Job ID
+        (out_dir / os.environ["SLURM_JOB_ID"]).touch()
+
+    all_data_weights = []
+    for i_bl, antpair in enumerate(uvd.get_antpairs()):
+        bl_str = f"{antpair[0]}-{antpair[1]}"
+
+        # Get visibility data with shape (Ntimes, Nfreqs)
+        # pI visibilities stored in the XX polarization
+        d = uvd.get_data(antpair + ("xx",), force_copy=True)
+
+        bl_data_shape = d.shape
+        cov_ff_shape = (Nfreqs, Nfreqs)
+        fgmodes_shape = (Nfreqs, args.Nfgmodes)
+
+        if args.flags:
+            flags_path = Path(args.flags)
+            flags_path_is_dir, flags = check_load_path(flags_path)
+            if flags_path_is_dir:
+                bl_flags_path = flags_path / bl_str / args.flags_file
+                flags = np.load(bl_flags_path)
+            check_shape(flags.shape, bl_data_shape, desc=f"flags")
+        else:
+            # FIXME: there is nothing currently in place to handle flags which
+            # differ between polarizations in `form_pseudo_stokes_vis`. 
+            # Differing flags should be accounted for in the future, possibly
+            # via Nsamples.
+            flags = uvd.get_flags(antpair + ("xx",))
+
+        if args.nsamples:
+            nsamples_path = Path(args.nsamples)
+            nsamples_path_is_dir, nsamples = check_load_path(nsamples_path)
+            if nsamples_path_is_dir:
+                bl_nsamples_path = nsamples_path / bl_str / args.nsamples_file
+                nsamples = np.load(bl_nsamples_path)
+            check_shape(nsamples.shape, bl_data_shape, desc=f"nsamples")
+        else:
+            nsamples = None
+
+        if args.noise:
+            noise_path = Path(args.noise)
+            noise_path_is_dir, noise = check_load_path(noise_path)
+            if noise_path_is_dir:
+                bl_noise_path = noise_path / bl_str / args.noise_file
+                noise = np.load(bl_noise_path)
+            check_shape(noise.shape, bl_data_shape, desc=f"noise")
+            if nsamples is not None:
+                # Approximate the reduction in the noise from averaging
+                # together many data points by dividing by Nsamples
+                noise /= np.sqrt(nsamples)
+            # If passing an array of noise, the data should be noiseless
+            d += noise
+
+        if args.sigcov0:
+            sigcov0_path = Path(args.sigcov0)
+            sigcov0_path_is_dir, sigcov0 = check_load_path(sigcov0_path)
+            if sigcov0_path_is_dir:
+                bl_sigcov0_path = sigcov0_path / bl_str / args.sigcov0_file
+                sigcov0 = np.load(bl_sigcov0_path)
+            check_shape(sigcov0.shape, cov_ff_shape, desc="signal covariance")
+        else:
+            sigcov0 = np.eye(Nfreqs)
+
+        if args.noise_cov:
+            noise_cov_path = Path(args.noise_cov)
+            noise_cov_path_is_dir, noise_cov = check_load_path(noise_cov_path)
+            if noise_cov_path_is_dir:
+                bl_noise_cov_path = (
+                    noise_cov_path / bl_str / args.noise_cov_file
+                )
+                noise_cov = np.load(bl_noise_cov_path)
+            check_shape(noise_cov.shape, cov_ff_shape, desc="noise covariance")
+            Ninv = np.linalg.inv(noise_cov)
+        else:
+            Ninv = np.eye(Nfreqs)
+
+        if args.fgmodes:
+            fgmodes_path = Path(args.fgmodes)
+            fgmodes_path_is_dir, fgmodes = check_load_path(fgmodes_path)
+            if fgmodes_path_is_dir:
+                if not args.fgmodes_file:
+                    # Look for a file with the default filename from
+                    # hydra-pspec/scripts/calc-vis-cov-matrices.py
+                    fgmodes_path = (
+                        fgmodes_path / bl_str / f"evecs-{freq_str}.npy"
+                    )
+                else:
+                    fgmodes_path = fgmodes_path / bl_str / args.fgmodes_file
+                fgmodes = np.load(fgmodes_path)
+            fgmodes = fgmodes[:, :args.Nfgmodes]
+            check_shape(fgmodes.shape, fgmodes_shape, desc="fgmodes")
+        else:
+            # Generate approximate set of FG modes from Legendre polynomials
+            fgmodes = np.array([
+                scipy.special.legendre(i)(np.linspace(-1., 1., freqs.size))
+                for i in range(args.Nfgmodes)
+            ]).T
+
+        bl_data_weights = {
+            "antpair": antpair,
+            "d": d,
+            "w": flags,
+            "fgmodes": fgmodes,
+            "S_initial": sigcov0,
+            "Ninv": Ninv,
+            "out_dir": out_dir
+        }
+        if args.sigcov0:
+            bl_data_weights["S_initial"] = sigcov0
+        if args.noise_cov:
+            bl_data_weights["N"] = noise_cov
+        all_data_weights.append(bl_data_weights)
 else:
-    # Generate approximate set of FG modes from Legendre polynomials
-    fgmodes = np.array([
-        scipy.special.legendre(i)(np.linspace(-1., 1., freqs.size))
-        for i in range(args.Nfgmodes)
-    ]).T
+    all_data_weights = None
+
+# Send per-baseline visibilities to each process
+data = comm.scatter(all_data_weights)
+antpair = data["antpair"]
+d = data["d"]
+w = ~data["w"]
+fgmodes = data["fgmodes"]
+S_initial = data["S_initial"]
+Ninv = data["Ninv"]
+
+# Create a subdirectory in out_dir for each baseline
+out_dir = data["out_dir"]
+bl_str = f"{antpair[0]}-{antpair[1]}"
+out_dir /= bl_str
+out_dir.mkdir(exist_ok=True, parents=True)
 
 # Power spectrum prior
 # This has shape (2, Ndelays). The first dimension is for the upper and
 # lower prior bounds respectively. If the prior for a given delay is
 # set to zero, no prior is applied. Otherwise, the solution is restricted
 # to be within the range ps_prior[1] < soln < ps_prior[0].
+Nfreqs = d.shape[1]
 ps_prior = np.zeros((2, Nfreqs))
 if args.ps_prior_lo != 0 or args.ps_prior_hi != 0:
     ps_prior_inds = slice(
@@ -395,31 +479,14 @@ if args.ps_prior_lo != 0 or args.ps_prior_hi != 0:
     ps_prior[0, ps_prior_inds] = args.ps_prior_hi
     ps_prior[1, ps_prior_inds] = args.ps_prior_lo
 
-# Output file set up
-out_dir = Path(args.out_dir)
-if not args.dirname:
-    out_dir /= f"results-{freq_str}-Niter-{args.Niter}"
+if rank == 0:
+    verbose = args.verbose
 else:
-    if args.map_estimate:
-        out_dir /= args.dirname + "-map-estimate"
-    else:
-        out_dir /= args.dirname
-if out_dir.exists() and not args.clobber:
-    # Check for existing output files to avoid overwriting if clobber=False
-    add_mtime_to_filepath(out_dir)
-out_dir /= bl_str
-out_dir.mkdir(exist_ok=True, parents=True)
-# Catalog git version
-try:
-    git_info = get_git_version_info()
-except:
-    git_info = None
-with open(out_dir / "git.json", "w") as f:
-    # Catalog git version info
-    json.dump(git_info, f)
-# Catalog command line arguments
-parser.save(args, out_dir / "args.json", format="json", skip_none=False)
-print(f"\nWriting output(s) to {out_dir.absolute()}", end="\n\n")
+    verbose = False
+if verbose:
+    print("Printing status messages for:")
+    print(f"Rank:     {rank}")
+    print(f"Baseline: {antpair}", end="\n\n")
 
 # Run Gibbs sampler
 # signal_cr = (Niter, Ntimes, Nfreqs) [complex]
@@ -430,15 +497,15 @@ start = time.time()
 signal_cr, signal_S, signal_ps, fg_amps, chisq, ln_post = \
     hp.pspec.gibbs_sample_with_fg(
         d,
-        ~flags[0],  # FIXME: add functionality for time-dependent flags
-        sigcov0,
+        w[0],  # FIXME: add functionality for time-dependent flags
+        S_initial,
         fgmodes,
         Ninv,
         ps_prior,
         Niter=args.Niter,
         seed=args.seed,
         map_estimate=args.map_estimate,
-        verbose=args.verbose,
+        verbose=verbose,
         nproc=args.Nproc,
         write_Niter=args.write_Niter,
         out_dir=out_dir
