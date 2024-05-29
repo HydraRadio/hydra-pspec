28d418a26cf988b37858b7921b7fdc589cb16106
Gerard Capes <gerard.capes@manchester.ac.uk>
2024-05-29 16:12:20 +0100

    Move slurm logs directory
    The speed up plot expects all subdirectories to be results.
    Having a logs directory makes it crash, and this is easier than
    excluding directories in the plotting script.

--- create_jobscript.sh
+++ create_jobscript.sh
@@ -8,4 +8,4 @@ n_baselines=3
 results_dir=strong_scaling_results
 sed "s/n_ranks/${n_ranks}/g; s/job_name/${job_name}/g; s/out_dir/${results_dir}/g" \
     jobscript.sh.template > jobscript_${job_name}.sh
-mkdir -p ${results_dir}/slurm-logs
+mkdir -p slurm-logs
