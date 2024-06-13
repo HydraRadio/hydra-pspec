5f38795c082d7b7ca4b88e9ace34255792590bb1
Gerard Capes <gerard.capes@manchester.ac.uk>
2024-06-13 14:16:42 +0100

    Load all timings rather than just one

--- plot_speed_up.py
+++ plot_speed_up.py
@@ -21,22 +21,22 @@ args = parser.parse_args()
 
 if args.results_dir:
     # Combine files from multiple runs
-    timings = []
+    timing_logs = []
     results_dir = Path(args.results_dir).resolve()
     for dir_item in results_dir.iterdir():
         if dir_item.is_dir():
             file = dir_item.joinpath("timings.json")
             with open(file) as f:
                 data = json.load(f)
-            timings.append(data)
+            timing_logs.append(data)
 
     with open(results_dir.joinpath("combined_timings.json"), "w") as f:
         # Save summary file
-        json.dump(timings, f, indent=2)
+        json.dump(timing_logs, f, indent=2)
 
 if args.summary_file:
     with open(Path(args.summary_file).resolve()) as f:
-        timings = json.load(f)
+        timing_logs = json.load(f)
     results_dir = Path(args.summary_file).parent.resolve()
 
 
@@ -52,20 +52,40 @@ def plot_speed_up_ranks(speed_up: list, n_ranks: list):
     plt.savefig(results_dir.joinpath("speed_up.svg"))
 
 
-def process_timings(data: list[dict], timer: str):
+def process_timings(data: list[dict]):
     "Extract execution time and number of ranks"
     n_ranks = []
-    ex_time = []
+    time_load = []
+    time_scatter = []
+    time_process = []
+    time_barrier = []
+    time_total = []
+
     for d in data:
         n_ranks.append(d["num_ranks"])
-        ex_time.append((d["rank_0_timers"][timer]))
+        time_load.append((d["rank_0_timers"]["load_data"]))
+        time_scatter.append((d["rank_0_timers"]["scatter"]))
+        time_process.append((d["rank_0_timers"]["process"]))
+        time_barrier.append((d["rank_0_timers"]["barrier"]))
+        time_total.append((d["rank_0_timers"]["total"]))
 
     sorted_indices = sorted(range(len(n_ranks)), key=lambda i: n_ranks[i])
-    ex_time = [ex_time[i] for i in sorted_indices]
+    time_load = [time_load[i] for i in sorted_indices]
+    time_scatter = [time_scatter[i] for i in sorted_indices]
+    time_process = [time_process[i] for i in sorted_indices]
+    time_barrier = [time_barrier[i] for i in sorted_indices]
+    time_total = [time_total[i] for i in sorted_indices]
     n_ranks.sort()
-    speed_up = [ex_time[0]/t for t in ex_time]
+    speed_up = [time_total[0]/t for t in time_total]
 
-    return ex_time, n_ranks, speed_up
+    timings = {"load": time_load,
+              "scatter": time_scatter,
+              "process": time_process,
+              "barrier": time_barrier,
+              "total": time_total,
+              "n_ranks": n_ranks,
+              "speed_up": speed_up}
+    return timings
 
 
 def plot_time_vs_ranks(ex_time: list, x: list):
@@ -79,11 +99,12 @@ def plot_time_vs_ranks(ex_time: list, x: list):
     plt.legend()
     plt.savefig(results_dir.joinpath("time_vs_ranks.svg"))
 
+
 if args.timer:
     timer = args.timer
 else:
     timer = "total"
 
-ex_time, n_ranks, speed_up = process_timings(timings, timer)
-plot_time_vs_ranks(ex_time, n_ranks)
-plot_speed_up_ranks(speed_up, n_ranks)
+timings = process_timings(timing_logs)
+plot_time_vs_ranks(timings["total"], timings["n_ranks"])
+plot_speed_up_ranks(timings["speed_up"], timings["n_ranks"])
