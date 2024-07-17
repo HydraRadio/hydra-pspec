e0c31b0d9ac3c1e8005189d6ba89fdfa8ee88dee
Gerard Capes <gerard.capes@manchester.ac.uk>
2024-07-17 08:35:26 +0100

    Tidy up job names

--- create_jobscript.sh
+++ create_jobscript.sh
@@ -3,7 +3,7 @@
 # bash create_jobscript.sh number_of_ranks
 
 n_ranks=$1
-job_name="strongscaling_${n_ranks}ranks"
+job_name="${n_ranks}ranks_strongscaling"
 results_dir=results/strong_scaling
 sed "s/@n_ranks@/${n_ranks}/g; s/@job_name@/${job_name}/g; s#@out_dir@#"${results_dir}"#g" \
     jobscript.sh.template > jobscript_${job_name}.sh
