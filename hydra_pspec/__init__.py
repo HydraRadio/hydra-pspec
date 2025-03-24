6dac6c9987de4c14c1eaa59b8a3299f5c8e5680c
jburba <jacob.burba@manchester.ac.uk>
2025-03-24 17:36:52 +0000

    Set __version__ in hydra_pspec/__init__.py

--- hydra_pspec/__init__.py
+++ hydra_pspec/__init__.py
@@ -1,1 +1,12 @@
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    from ._version import version as __version__
+except ModuleNotFoundError:  # pragma: no cover
+    try:
+        __version__ = version("hydra_pspec")
+    except PackageNotFoundError:
+        # package is not installed
+        __version__ = "unknown"
+
 from . import dpss, lssa, oqe, pspec, utils
